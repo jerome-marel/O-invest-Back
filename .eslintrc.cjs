cb236f9e5d6f349448d4f395f64f214eafcf1cfd
Razack Alibay Kandjy <razack@MBP-Razack.local>
2023-08-22 15:02:26 +0200

    instal eslint airbnb

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": ["airbnb-base"], // car the Airbnb ESLint config est installé
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+}
