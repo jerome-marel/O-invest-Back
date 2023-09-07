d5e1384c44cdfbaf7269c8a681fc8d3ad7aaeef1
Alex Tensorer <alexandre.tensorer@oclock.school>
2023-09-07 10:22:52 +0200

    update prices 2 mins on dashboard

--- app/validation/schemas/addAsset.schema.js
+++ app/validation/schemas/addAsset.schema.js
@@ -4,7 +4,7 @@ const addAssetSchema = Joi.object({
   symbol: Joi.string().required(),
   purchaseDatetime: Joi.date().required(),
   quantity: Joi.number().required(),
-  note: Joi.string().optional(),
+  note: Joi.string().allow('').optional(),
 });
 
 export default addAssetSchema;
--- app/validation/schemas/portfolio.schema.js
+++ app/validation/schemas/portfolio.schema.js
@@ -1,8 +1,9 @@
 import Joi from 'joi';
 
 const portfolioSchema = Joi.object({
+  userId: Joi.number().required(),
   name: Joi.string().required(),
-  strategy: Joi.string().required(),
+  strategy: Joi.string().allow('').optional(),
 });
 
 export default portfolioSchema;
